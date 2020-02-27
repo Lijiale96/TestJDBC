2a61615bf5fac3d5f85ed4dfc5f1e144185a603f
Lijiale96 <ljl512@163.com>
2020-02-27 20:03:51 +0800

    3

--- src/com/bjsxt/jdbc/Demo10.java
+++ src/com/bjsxt/jdbc/Demo10.java
@@ -7,10 +7,9 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 /**
- * 测试CLOB的文本大对象的使用
- * 包含：将字符串、文件内容插入数据库中的CLOB字段、将ClOB字段值取出来
+ * 测试BLOB 二进制 大对象的使用
  */
-public class Demo09 {
+public class Demo10 {
 
     public static long str2Date(String dateStr){
         DateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
@@ -27,7 +26,10 @@ public class Demo09 {
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
-        Reader r =null;
+        //Reader r =null;
+        InputStream is = null;
+        OutputStream os = null;
+
         //加载驱动类
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -36,22 +38,26 @@ public class Demo09 {
                      "root","520512");
 
 
-           //ps = conn.prepareStatement("insert into t_user2 (username,myInfo) value(?,?)");
-            //ps.setString(1,"阿康");
-          //  ps.setClob(2,new FileReader(new File("d:/a.txt")));  //将文本内容直接输入到数据库中
-            // 将程序中的字符串输入到数据库的CLOB字段中
-        //    ps.setClob(2, new BufferedReader(new InputStreamReader(new ByteArrayInputStream("handsome".getBytes()))));
+//           ps = conn.prepareStatement("insert into t_user2 (username,headImg) value(?,?)");
+//            ps.setString(1,"阿康");
+//            ps.setBlob(2,new FileInputStream("d:/icon.jpg"));
+//            ps.execute();
 
+//            ps.setClob(2,new FileReader(new File("d:/a.txt")));  //将文本内容直接输入到数据库中
+//            // 将程序中的字符串输入到数据库的CLOB字段中
+//            ps.setClob(2, new BufferedReader(new InputStreamReader(new ByteArrayInputStream("handsome".getBytes()))));
+//
             ps = conn.prepareStatement("select * from t_user2 where id =?");
-            ps.setObject(1,112023);
+            ps.setObject(1,112026);
 
             rs = ps.executeQuery();
             while (rs.next()){
-               Clob c =  rs.getClob("myInfo");
-                r = c.getCharacterStream();
+               Blob b =  rs.getBlob("headImg");
+                is = b.getBinaryStream();
+                os= new FileOutputStream("d:/a.jpg");
                int temp =0;
-               while((temp=r.read())!=-1){
-                   System.out.print((char)temp);
+               while((temp=is.read())!=-1){
+                  os.write(temp);
                }
             }
 
@@ -63,18 +69,22 @@ public class Demo09 {
             e.printStackTrace();
         }finally {
             try {
-                if (r != null) {
-                    r.close();
+                if (is != null) {
+                    is.close();
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
                 try {
-                    if (conn != null) {
-                        conn.close();
+                    if (os != null) {
+                        try {
+                            os.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
-                } catch (SQLException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
         }
